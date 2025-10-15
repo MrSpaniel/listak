dad1e7ddcdca05e88d785b0db75779f2032c684f
Fehér Dániel <feher.daniel.sandor@diak.szbi-pg.hu>
2025-10-15 08:50:21 +0200

    elem hozzaadasa es torlese

--- lista.py
+++ lista.py
@@ -9,3 +9,11 @@ print("Utolsó hónap törölve:","\n",
 torolt_honap = honapok.pop()
 print("Törölt hónap:",torolt_honap)
 print(honapok)
+
+torolt_honap = honapok.pop(0)
+print(honapok)
+
+print(honapok.remove("februar"))
+honapok.clear()
+print(honapok)
+honapok.insert(0, "februar")
