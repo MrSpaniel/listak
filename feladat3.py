dcd29057f4889821637c563360ef1bbfd6b89d66
Fehér Dániel <feher.daniel.sandor@diak.szbi-pg.hu>
2025-10-22 08:49:25 +0200

    paros harommal

--- feladat3.py
+++ feladat3.py
@@ -1,3 +1,7 @@
 """Az adott lista (→ 'Próbáld ki!') elemei közül a program kiírja a 3-mal osztható páros számokat!
 """
-
+paros = []
+for i in range(1, 101):
+    if i % 3 == 0 and i % 2 == 0:
+        paros.append(i)
+print(paros)
